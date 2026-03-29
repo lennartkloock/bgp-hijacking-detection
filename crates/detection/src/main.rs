8a061dd30f251fd2f303834bf2a994b7fe040c98
Lennart Kloock <lennart.kloock@protonmail.com>
2026-03-29 20:06:27 +0200

    feat(detection): use jemallocator

--- crates/detection/src/main.rs
+++ crates/detection/src/main.rs
@@ -1,6 +1,13 @@
 use detection::global::Global;
 use detection::service::DetectionSvc;
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 scuffle_bootstrap::main! {
     Global {
         scuffle_signal::SignalSvc,
